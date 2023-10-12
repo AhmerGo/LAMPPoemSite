07050f83654c582ad6ef0fe69a494bb943867172
Ahmer Gondal <ajg2204@hsutx.edu>
2023-10-12 16:56:31 -0500

    PHP Mailer Library added to config

--- email_config.php
+++ email_config.php
@@ -1,12 +1,16 @@
 <?php
 // email_config.php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+require 'vendor/autoload.php';
 return [
     'host' => 'smtp.gmail.com',
     'username' => 'your-email@gmail.com',
     'password' => 'your-password',
     'smtp_secure' => PHPMailer::ENCRYPTION_STARTTLS, // or PHPMailer::ENCRYPTION_SMTPS
-    'port' => 587, // or 465 for SMTPS
+    'port' => 587, 
     'from_email' => 'from@example.com',
     'from_name' => 'Mailer'
 ];
